ea3895f7190d207dd8b05bc69689c22412ff6183
Edebo <akandeadebowale0@gmail.com>
2020-02-28 23:12:36 +0100

    validate inputs

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ let vm = new Vue({
     title: "Simple Interest Calculator",
     principal: 0,
     rate: 0,
-    time: 0
+    time: 0,
+    error: false
   },
   methods: {},
   computed: {
     interest: function() {
+      this.principal = this.principal < 0 ? 0 : this.principal;
+      this.rate = this.rate < 0 ? 0 : this.rate;
+      this.time = this.time < 0 ? 0 : this.time;
+
       return (this.principal * this.rate * this.time) / 100;
     }
   }
